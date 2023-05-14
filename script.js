38ea5ec468ddc9d86f611efb33e5b10abb94c2d5
zakee21 <zakee21@gmail.com>
2023-05-13 18:46:09 -0700

    Style with CSS, Winner announced after obtaining 5 points

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-//Variables for user score and computer's score
+
+//Global variables for user score and computer's score
 let userScore = 0;
 let compScore = 0; 
 
@@ -9,62 +10,56 @@ function getComputerChoice() {
     return choice[random];
     }
 
-//function to play one round of rock, paper, scissors.
-function playRound(userSelection) {
+//function to play one round of rock paper scissors.
+function playRound(userSelection) {    
 
-/*User selects rock, paper or scissors, case instensitive
-    const choice = prompt("Enter your choice: ");
-    const userSelection = choice.toLowerCase();*/
-    console.log("Your Selection: " + userSelection);
+    //Display user's selection in html
+    const userChoice = document.querySelector('#userChoice');
+    userChoice.textContent = 'Your Choice : ' + userSelection;
 
-//Obtain computers selection.
+    //Invoke function for computer's random selection. Obtain computers selection and display it.
     const compSelection = getComputerChoice();
-    console.log("Computer's Selection: " + compSelection);  
+    const compChoice = document.querySelector('#compChoice');
+    compChoice.textContent = "Computer's Choice : " + compSelection;
 
-//Compare user selection with computer selection, add points to the winner's score.
+    //Compare user selection with computer selection, add points to the winner's score, display results for each round.
     if (userSelection === compSelection) {
-        console.log("You: ", userScore, "\nComputer: ", compScore);
-        return console.log("It's a tie!");
+        document.querySelector('#userScore').textContent = "You : " + userScore;
+        document.querySelector('#compScore').textContent = "Computer : " + compScore;
+        return document.querySelector('#roundResult').textContent = "It's a tie!";
     }
     else if (userSelection === "rock" && compSelection === "scissors") {
         ++userScore;
-        console.log("You: ", userScore, "\nComputer: ", compScore);
-        return console.log("You win!");
+        document.querySelector('#userScore').textContent = "You : " + userScore;
+        document.querySelector('#compScore').textContent = "Computer : " + compScore;
+        document.querySelector('#roundResult').textContent = "You win!";       
+        
     }
     else if (userSelection === "paper" && compSelection === "rock") {
         ++userScore;
-        console.log("You: ", userScore, "\nComputer: ", compScore);
-        return console.log("You win!");
+        document.querySelector('#userScore').textContent = "You : " + userScore;
+        document.querySelector('#compScore').textContent = "Computer : " + compScore;
+        document.querySelector('#roundResult').textContent = "You win!";
+        
     }
     else if (userSelection === "scissors" && compSelection === "paper") {
         ++userScore;
-        console.log("You: ", userScore, "\nComputer: ", compScore);
-        return console.log("You win!");
+        document.querySelector('#userScore').textContent = "You : " + userScore;
+        document.querySelector('#compScore').textContent = "Computer : " + compScore;
+        document.querySelector('#roundResult').textContent = "You win!";
     }
     else {
-        compScore = ++compScore;
-        console.log("You: ", userScore, "\nComputer: ", compScore);
-        return console.log("You lose!");
+        ++compScore;
+        document.querySelector('#userScore').textContent = "You : " + userScore;
+        document.querySelector('#compScore').textContent = "Computer : " + compScore;
+        document.querySelector('#roundResult').textContent = "You lose!";
+    }
+
+    //announce if user wins or loses the game after the user or computer reaches 5 points
+    if (userScore >= 5) {
+        document.querySelector('#roundResult').textContent = "YOU WIN THE GAME!";
+    }
+    else if (compScore >= 5) {
+        document.querySelector('#roundResult').textContent = "YOU WIN THE GAME!";
     }
 } 
-
-//play 5 rounds of rock, paper, scissors and display score results and whether the use wins, loses, or if it is a tie game.
-//removed this for the new ui-rps branch of rock papper scissors repo.
-/*function game(){
-    for(let i = 0; i < 5; i++) {
-        playRound(i);
-    }
-    if (userScore > compScore) {
-        console.log("YOU WIN THE GAME!");
-    }
-    else if (userScore === compScore){
-        console.log("TIE GAME!")
-    }
-    else {
-        console.log("YOU LOST THE GAME!");
-    }
-    return console.log("FINAL SCORE:\n","You: ", userScore, "\nComputer: ", compScore);
-}
-
-//invoke function to start full game
-game();*/
